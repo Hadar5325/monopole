860345067b8a56110a342383577c211b2498c24d
Hadar5325 <55797369+Hadar5325@users.noreply.github.com>
2020-05-18 22:55:35 -0700

    wonderful!

--- FlowOfGame.java
+++ FlowOfGame.java
@@ -1,10 +1,8 @@
-import java.io.*;
 import java.lang.String;
-import java.util.Scanner;
 
 
 public class FlowOfGame {
-    public static int numberOfPlayer =1;
+    public static int numberOfPlayer = 1;
     private int sizeOfGame;
 
     FlowOfGame(int sizeOfGame) {
@@ -15,20 +13,32 @@ public class FlowOfGame {
 
 
     void printBoard() {
-            int[] position = new int[this.sizeOfGame];
-            orderOfBoard();
-            // moving action in game
-            player.movingforwordPlayer();
+        int pos;
+        int[] position = new int[this.sizeOfGame];
+        orderOfBoard();
+        // moving action in game
+        player.movingforwordPlayer();
+        System.out.printf("\n");
 
-           // after throwing the dices get the state without circle
-            int stateInGame = player.getStateInGame();
-            int afterCircleStateInGame = stateInGame % (this.sizeOfGame);
-            for(int pos=0; pos < sizeOfGame ; pos++){
-                if(pos == afterCircleStateInGame){
-                    System.out.println("["+ numberOfPlayer + "]");
-                }
+        // after throwing the dices get the state without circle
+        int stateInGame = player.getProceedInGame();
+        int afterCircleStateInGame = stateInGame % (this.sizeOfGame);
+        for (pos = 0; pos < sizeOfGame; pos++) {
+            if (pos == afterCircleStateInGame) {
+                System.out.println("[" + numberOfPlayer + "]");
+                break;
+            }else{
+                System.out.print("   ");
             }
+        }
+        System.out.println("dice number 1 is: " + player.getDice1());
+        System.out.println("dice number 2 is: " + player.getDice2());
+        System.out.println("Dice rolled " + player.getTotalDices() +
+                " and player " + numberOfPlayer + " landed on square " + pos);
+        // TODO ------ FIX THE TAX
+        System.out.println("player "+ numberOfPlayer+ " you paid a tax of ");
     }
+
 
     public void orderOfBoard() {
         // declaring array and allocating memory to array
@@ -36,7 +46,7 @@ public class FlowOfGame {
         String[] valueOfEachCell = new String[sizeOfGame];
         for (int i = 0; i < sizeOfGame; i++) {
             numArray[i] = i;
-            System.out.print(numArray[i] + " ");
+            System.out.print(numArray[i] + "  ");
         }
         System.out.println(" ");
 
@@ -49,35 +59,34 @@ public class FlowOfGame {
             // a tax space
             else if (i % 4 == 0) {
                 if (i > 9) {
-                    valueOfEachCell[i] = " T";
+                    valueOfEachCell[i] = "  T";
                 } else {
-                    valueOfEachCell[i] = "T";
+                    valueOfEachCell[i] = " T";
                 }
                 // a property space
             } else if (i % 3 == 0) {
                 if (i > 9) {
-                    valueOfEachCell[i] = " P";
+                    valueOfEachCell[i] = "  P";
                 } else {
-                    valueOfEachCell[i] = "P";
+                    valueOfEachCell[i] = " P";
                 }
                 // a lottery space
             } else if (i % 2 == 0) {
                 if (i > 9) {
-                    valueOfEachCell[i] = " L";
+                    valueOfEachCell[i] = "  L";
                 } else {
-                    valueOfEachCell[i] = "L";
+                    valueOfEachCell[i] = " L";
                     // an empty space
                 }
             } else {
                 if (i > 9) {
-                    valueOfEachCell[i] = " E";
+                    valueOfEachCell[i] = "  E";
                 } else {
-                    valueOfEachCell[i] = "E";
+                    valueOfEachCell[i] = " E";
                 }
             }
             System.out.print(valueOfEachCell[i] + " ");
         }
-        System.out.println("\n");
     }
 }
 
--- player.java
+++ player.java
@@ -2,41 +2,54 @@ import java.util.Random;
 
 public class player {
     private int moneyInPocket;
-    private int stateInGame;
+    private int dice1;
+    private int dice2;
+    private int sumDices;
+    private int proceedInGame;
+    FlowOfGame flowOfGame;
+
     public player()
     {
-        this.moneyInPocket = 100;
-        this.stateInGame = 0;
+        this.moneyInPocket = 100; // in dollars
+        this.proceedInGame = 0;
     }
 
     public void movingforwordPlayer(){
         Random random = new Random();
-        int dice1 = random.nextInt(5);
+        dice1 = random.nextInt(5);
         dice1 += 1;
-        int dice2 = random.nextInt(5);
+        dice2 = random.nextInt(5);
         dice2 += 1;
         int sumDices = dice1+ dice2;
-        this.stateInGame = sumDices;
+        this.sumDices = sumDices;
+        this.proceedInGame = sumDices;
     }
-
-
 
 
     // getters
     public int getMoneyInPocket(){
         return this.moneyInPocket;
     }
-    public int getStateInGame(){
-        return this.stateInGame;
+    public int getProceedInGame(){
+        return this.proceedInGame;
     }
 
     // setters
     public void setMoneyInPocket(int moneyInPocket){
         this.moneyInPocket = moneyInPocket;
     }
-    public void setStateInGame(int stateInGame){
-        this.stateInGame= stateInGame;
+    public void setProceedInGame(int proceedInGame){
+        this.proceedInGame = proceedInGame;
+    }
+    public int getDice1(){
+        return this.dice1;
+    }
+    public int getDice2(){
+        return this.dice2;
     }
 
+    public int getTotalDices(){
+        return this.sumDices;
+    }
 }
 
